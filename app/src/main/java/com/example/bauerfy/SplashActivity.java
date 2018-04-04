1794b7cef247439dcf74c307fea6101377d86848
Unknown <smk.vanbeurden@student.avans.nl>
2018-04-04 15:09:14 +0200

    Added login page and designed home page

--- app/src/main/java/com/example/bauerfy/SplashActivity.java
+++ app/src/main/java/com/example/bauerfy/SplashActivity.java
@@ -11,6 +11,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
